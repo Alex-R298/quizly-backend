18169a0950f1742f162f256c6e38d0b2aa38c7fa
Alex-R298 <alexreitz298@gmail.com>
2026-05-02 21:24:49 +0200

    first commit

---
+++ auth_app/api/views.py
@@ -0,0 +1,17 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+
+class RegisterView(APIView):
+    pass
+
+
+class LoginView(APIView):
+    pass
+
+
+class LogoutView(APIView):
+    pass
+
+
