ceb7850913765ef836d44b11a6dd882d798bb0a2
Alex-R298 <alexreitz298@gmail.com>
2026-05-04 18:08:31 +0200

    Add tests for quiz and question model creation

--- tests/test_quiz.py
+++ tests/test_quiz.py
@@ -2,6 +2,7 @@ from unittest.mock import patch
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.contrib.auth.models import User
+from quiz_app.models import Quiz, Question
 
 
 class GenerateQuizTestCase(APITestCase):
@@ -49,3 +50,33 @@ class GenerateQuizTestCase(APITestCase):
         mock_generate.side_effect = Exception("yt-dlp failed")
         response = self.client.post('/api/quizzes/', {"url": self.valid_url}, format='json')
         self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class QuizModelTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.force_authenticate(user=self.user)
+        self.quiz = Quiz.objects.create(
+            owner=self.user,
+            title="Test Quiz",
+            description="Test",
+        video_url="https://www.youtube.com/watch?v=example",
+    )
+        
+    def test_quiz_creation(self):
+        self.assertEqual(self.quiz.owner, self.user)
+        self.assertEqual(self.quiz.title, "Test Quiz")
+        self.assertEqual(self.quiz.description, "Test")
+        self.assertEqual(self.quiz.video_url, "https://www.youtube.com/watch?v=example")
+
+    def test_question_creation(self):
+        question = Question.objects.create(
+            quiz=self.quiz,
+            question_title="What is 2+2?",
+            question_options=["1", "2", "3", "4"],
+            answer="4",
+        )
+        self.assertEqual(question.quiz, self.quiz)
+        self.assertEqual(question.question_title, "What is 2+2?")
+        self.assertEqual(question.question_options, ["1", "2", "3", "4"])
+        self.assertEqual(question.answer, "4")
