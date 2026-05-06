fabb11144ddea202160895fdadddc28724c89112
Alex-R298 <alexreitz298@gmail.com>
2026-05-06 15:45:58 +0200

    feat: configure admin panel for Quiz and Question

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from quiz_app.models import Quiz, Question
+
+
+class QuestionInline(admin.TabularInline):
+    """Inline editor for questions within the quiz admin view."""
+
+    model = Question
+    extra = 0
+    fields = ['question_title', 'question_options', 'answer']
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    """Admin configuration for Quiz, including inline question editing."""
+
+    list_display = ['title', 'owner', 'created_at', 'updated_at']
+    search_fields = ['title', 'owner__username']
+    inlines = [QuestionInline]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    """Admin configuration for individual questions."""
+
+    list_display = ['question_title', 'quiz', 'answer']
+    search_fields = ['question_title', 'quiz__title']
